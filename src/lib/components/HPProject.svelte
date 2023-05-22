1265293d90297619e84a965a44e4dd5450605a02
Aylay <luk.tattali@gmail.com>
2023-05-22 14:38:23 +0200

    Project video

--- src/lib/components/HPProject.svelte
+++ src/lib/components/HPProject.svelte
@@ -32,6 +32,7 @@
 	on:inview_change={handleChange}
 >
 	{#if isInView}
+		{#if project.img.data.attributes.mime.includes('image')}
 		<img
 			src={strapiURL + project.img.data.attributes.url}
 			alt={project.img.data.attributes.alternativeText
@@ -39,6 +40,22 @@
 				: project.title}
 			class="w-full {isInView ? 'animate-fade' : 'opacity-0'}"
 		/>
+		{/if}
+		{#if project.img.data.attributes.mime.includes('video')}
+		<video
+			class="h-auto w-full {isInView ? 'animate-fade' : 'opacity-0'}"
+			loop
+			muted
+			playsinline
+			autoplay
+			controls={false}
+		>
+			<source
+				src={strapiURL + project.img.data.attributes.url}
+				type="video/mp4"
+			/>
+		</video>
+		{/if}
 	{/if}
 	<h4
 		class="text-[2rem] font-medium -tracking-[0.03em] {projectsLength === 1
