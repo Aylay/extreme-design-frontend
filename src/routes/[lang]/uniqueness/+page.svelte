38a0da3ae9dcd836cedb820a222a0f6cdde5d851
Aylay <luk.tattali@gmail.com>
2023-06-23 10:04:16 +0200

    uniqueness back

--- src/routes/[lang]/uniqueness/+page.svelte
+++ src/routes/[lang]/uniqueness/+page.svelte
@@ -1,16 +1,22 @@
 <script lang="ts">
+	import { inview } from 'svelte-inview';
+	import type { ObserverEventDetails, Options } from 'svelte-inview';
 	import SvelteMarkdown from 'svelte-markdown';
 
-	import {gsap}  from "gsap/dist/gsap";        
-	import {ScrollTrigger} from "gsap/dist/ScrollTrigger";    
-
 	import { page } from '$app/stores';
-	import { onMount, onDestroy } from 'svelte';
 
 	const strapiURL = import.meta.env.VITE_STRAPI_URL;
 	let content: any;
 
-	$: content = $page.data.content;
+	$: {
+		content = $page.data.content;
+	}
+
+	let isInView: boolean;
+	const options: Options = {
+		unobserveOnEnter: true,
+		rootMargin: '50px'
+	};
 
 	const mdOptions = {
 		breaks: true,
@@ -18,66 +24,45 @@
 		headerIds: false
 	};
 
-	onMount(() => {
-		if(typeof window !== "undefined"){
-			gsap.registerPlugin(ScrollTrigger)
-		}
-
-		const scroller = document.body;
-
-		ScrollTrigger.defaults({ 
-			scroller: scroller,
-			pinType: 'transform'
-		});
-		
-		const timeline1 = gsap.timeline({
-			scrollTrigger: {
-				id: 'uniqueness',
-        trigger: '.title',
-        start: 'top top',
-        end: '+=250%',
-        scrub: true,
-        pin: true,
-        anticipatePin: 1
-			}
-		})
-		timeline1.to('.title p', { duration: 0.5, x: '-105%' })
-		timeline1.to('.title', { duration: 0.1, opacity: 0 }, 'end')
-		timeline1.to('.content', { duration: 0.15, y: '-50%' }, 'end')
-		timeline1.to('.title p', { duration: 0.15, y: '-300px' }, 'end')
-	});
-
-	onDestroy(() => {
-		ScrollTrigger.refresh()
-	})
+	const handleChange = ({ detail }: CustomEvent<ObserverEventDetails>) => {
+		isInView = detail.inView;
+	};
 </script>
 
-<div class="title h-screen flex justify-center items-center overflow-x-hidden">
-	<div class="max-lg:pl-[16px] flex flex-nowrap gap-[26px] pl-[48px] w-full">
-		<p class="text-[60px] font-medium -tracking-[0.03em] lg:text-[212px]">{content.title}</p>
-		<p class="text-[60px] font-medium -tracking-[0.03em] lg:text-[212px]">{content.title}</p>
+<div
+	class="flex flex-nowrap gap-[26px] overflow-hidden pb-[56px] pl-[48px] pt-[90px] max-lg:pl-[16px] lg:pb-[96px] lg:pt-[232px]"
+>
+	<p class="text-[60px] font-medium -tracking-[0.03em] lg:text-[212px]">{content.title}</p>
+	<p class="text-[60px] font-medium -tracking-[0.03em] lg:text-[212px]">{content.title}</p>
+	<p class="text-[60px] font-medium -tracking-[0.03em] lg:text-[212px]">{content.title}</p>
+	<p class="text-[60px] font-medium -tracking-[0.03em] lg:text-[212px]">{content.title}</p>
+</div>
+
+<div class="mb-[56px] gap-[48px] px-[48px] max-lg:px-[16px] lg:mb-[96px] lg:grid lg:grid-cols-4">
+	<div class="col-span-1 max-lg:hidden" />
+	<div
+		class="flex flex-col gap-[154px] text-[32px] leading-[36px] -tracking-[0.03em] lg:col-span-2 lg:gap-[112px] lg:text-[40px] lg:leading-[46px]"
+	>
+		<SvelteMarkdown source={content.text} options={mdOptions} />
 	</div>
 </div>
 
-<div class="content relative -top-2/4">
-	<div class="mb-[56px] gap-[48px] px-[48px] max-lg:px-[16px] lg:mb-[96px] lg:grid lg:grid-cols-5">
-		<div class="col-span-1 max-lg:hidden" />
-		<div
-			class="flex flex-col gap-[154px] text-[32px] leading-[36px] -tracking-[0.03em] lg:col-span-3 lg:gap-[112px] lg:text-[40px] lg:leading-[46px]"
-		>
-			<SvelteMarkdown source={content.text} options={mdOptions} />
-		</div>
-	</div>
-	
-	{#if content.img && content.img.data}
-		<div class="max-lg:mb-[56px] max-lg:h-[360px] lg:mb-[-60%]">
+{#if content.img}
+	<div
+		class="mb-[56px] max-lg:h-[360px] lg:mb-[96px]"
+		use:inview={options}
+		on:inview_change={handleChange}
+	>
+		{#if isInView}
 			<img
 				src={strapiURL + content.img.data.attributes.url}
 				alt={content.img.data.attributes.alternativeText
 					? content.img.data.attributes.alternativeText
 					: content.title}
-				class="w-full max-lg:h-full max-lg:object-cover lg:h-auto"
+				class="w-full max-lg:h-full max-lg:object-cover lg:h-auto {isInView
+					? 'animate-fade'
+					: 'opacity-0'}"
 			/>
-		</div>
-	{/if}
-</div>
+		{/if}
+	</div>
+{/if}
