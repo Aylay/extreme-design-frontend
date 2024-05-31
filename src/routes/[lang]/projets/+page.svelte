be91cf16c1ed8b735a754944cfdd7844024f0f0d
Aylay <luk.tattali@gmail.com>
2024-05-31 21:26:42 +0200

    -

--- src/routes/[lang]/projets/+page.svelte
+++ src/routes/[lang]/projets/+page.svelte
@@ -79,19 +79,17 @@
 		</video>
 	{/if}
 </div>
-{:else}
-<div class="max-lg:hidden lg:h-[70px]" />
-{/if}
 
-{#if (content.title && content.title !== '') || (content.bigTitle && content.bigTitle !== '')}
-<div class="flex justify-between gap-[8px] px-[16px]  max-lg:flex-col lg:gap-[48px] lg:px-[48px]  {content.mainMediaDesktop && content.mainMediaDesktop.data && content.mainMediaMobile && content.mainMediaMobile.data ? 'pb-[56px] lg:pb-[96px]' : 'py-[56px] lg:py-[96px]'}">
-	<h1 class="flex-1 max-lg:text-m1 lg:text-medium lg:font-medium">
-		{@html content.bigTitle}
-	</h1>
-	<h2 class="flex-1 max-lg:text-m2 lg:text-medium">
-		{@html content.title}
-	</h2>
-</div>
+	{#if (content.title && content.title !== '') || (content.bigTitle && content.bigTitle !== '')}
+	<div class="flex justify-between gap-[8px] px-[16px]  max-lg:flex-col lg:gap-[48px] lg:px-[48px] pb-[56px] lg:pb-[96px]">
+		<h1 class="flex-1 max-lg:text-m1 lg:text-medium lg:font-medium">
+			{@html content.bigTitle}
+		</h1>
+		<h2 class="flex-1 max-lg:text-m2 lg:text-medium">
+			{@html content.title}
+		</h2>
+	</div>
+	{/if}
 {/if}
 
 {#if content.projectsList.data.length > 0}
